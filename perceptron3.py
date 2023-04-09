162131ea7b26c1244822f08a70577d8198f04548
LeonardoGomes99 <leogomes16477@gmail.com>
2023-04-08 21:52:45 -0300

    .

---
+++ perceptron3.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  8 21:24:58 2023
+
+@author: Leo
+"""
+import numpy as np
+
+entradas = np.array([[0,0],[0,1],[1,0],[1,1]])
+pesos = np.array([0.0, 0.0])
+saidas = np.array([0,0,0,1])
+taxaAprendizagem = 0.1
+
+def stepFunction(soma):
+    if (soma > 1):
+        return 1
+    return 0
+
+def calculaSaida(registro):
+    s = registro.dot(pesos)
+    return stepFunction(s)
+
+def treinar():
+    erroTotal = 1
+    while(erroTotal != 0):
+        erroTotal = 0        
+        for i in range(len(saidas)):
+            saidaCalculada = calculaSaida(np.asarray(entradas[i]))
+            erro = abs(saidas[i] - saidaCalculada)
+            erroTotal += erro
+            for j in range(len(pesos)):
+                pesos[j] = pesos[j] + (taxaAprendizagem * entradas[i][j] * erro)
+                print("Peso Atualizado: " + str(pesos[j]))
+        print('Total de Erros: ' + str(erroTotal))
+        
+treinar()
+print('Rede Neural')
+print(calculaSaida(entradas[0]))
+print(calculaSaida(entradas[1]))
+print(calculaSaida(entradas[2]))
+print(calculaSaida(entradas[3]))
